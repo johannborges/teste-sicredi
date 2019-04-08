f43903c8286e20b6d5b6d02e569918911a1dc3a3
Johann Borges <johannborges@hotmail.com>
2019-04-08 11:46:56 -0300

    base do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+import { SharedModule } from './shared/shared.module';
+import { InterceptorModule } from './interceptor/interceptor.module';
 
 import { AppComponent } from './app.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    }
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    HttpClientModule,
+    InterceptorModule,
+
+    RouterModule.forRoot(
+        routes,
+        { useHash: true }
+    )
   ],
-  providers: [],
+  providers: [HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
